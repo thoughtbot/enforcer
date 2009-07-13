da6692038ea818f76afbeb5d1dc66151a32ae9da
Corey Haines <coreyhaines@gmail.com>
2009-07-13 16:24:32 -0400

    Parsing json
    Signed-off-by: Nick Quaranto <nick@quaran.to>

--- lib/github_api.rb
+++ lib/github_api.rb
@@ -1,5 +1,13 @@
+require 'httparty'
+require 'json'
+
 class GitHubApi
-  def self.add_collaborator(repo, collaborator)
-    ["qrush", collaborator]
+  include HTTParty
+  base_uri 'http://github.com/api/v2/json'
+
+  def self.add_collaborator(account, api_key, repo, collaborator)
+    collaborator_list = self.post "/repos/collaborators/#{repo}/add/#{collaborator}", :login => account, :token => api_key
+    collaborators = JSON.parse(collaborator_list)
+    collaborators['collaborators']
   end
 end
--- test/github_api_test.rb
+++ test/github_api_test.rb
@@ -1,18 +1,36 @@
 require 'test_helper'
 
 class GitHubApiTest < Test::Unit::TestCase
-
-  context "adding collaborators" do
+  context "with github api credentials" do
     setup do
-      @repo = "repo"
-      @user = "coreyhaines" + rand(10).to_s
+      @account = "thoughtbot"
+      @api_key = "deadbeef"
+      @repo    = "repo"
+      @user    = "coreyhaines" + rand(10).to_s
       @collaborators = '{"collaborators": ["qrush", "'+ @user +'"]}'
-      stub(GitHubApi).post("http://github.com/api/v2/json/repos/collaborators/#{@repo}/add/#{@user}") { @collaborators }
+      stub(GitHubApi).post(anything, anything) { @collaborators }
     end
 
-    should "hit the github api" do
-      collaborators = GitHubApi.add_collaborator(@repo, @user)
-      assert_equal ["qrush",@user], collaborators
+    should "set base uri" do
+      assert_equal "http://github.com/api/v2/json", GitHubApi.base_uri
+    end
+
+    context "adding a collaborator" do
+      setup do
+        @collaborators = GitHubApi.add_collaborator(@account, @api_key, @repo, @user)
+      end
+
+      should "hit the github api" do
+        assert_received(GitHubApi) do |subject|
+          subject.post("/repos/collaborators/#{@repo}/add/#{@user}",
+            :login => @account,
+            :token => @api_key)
+        end
+      end
+
+      should "return the new collaborators" do
+        assert_equal ["qrush", @user], @collaborators
+      end
     end
   end
 
